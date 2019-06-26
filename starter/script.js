bce24417aa693099a4cb77a73d9d2f3e72d73fbf
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-06-26 22:53:47 +0100

    Variable types
    Demo

---
+++ starter/script.js
@@ -0,0 +1,23 @@
+console.log('Hello Rodolfo');
+
+//VARIABLES
+// 5 TYPES: NUMBER, STRING, BOOLEAN, UNDEFINED, NULL
+
+var firstName = 'Rodolfo';
+console.log(firstName);
+
+var lastName = 'Carvajal';
+var age = 32;
+
+var fullAge = true;
+console.log(fullAge);
+
+// UNDEFINED VARIBALE
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+
+console.log(job);
+
