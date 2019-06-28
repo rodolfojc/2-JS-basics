fc1d735bfebf240c2fda294c4b480aa6b2694057
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-06-28 22:20:42 +0100

    Varibale mutation and coercion
    Demo

--- starter/script.js
+++ starter/script.js
@@ -1,3 +1,6 @@
+/*
+VARIABLES AND DATA TYPE
+
 console.log('Hello Rodolfo');
 
 //VARIABLES
@@ -20,4 +23,30 @@ console.log(job);
 job = 'Teacher';
 
 console.log(job);
+*/
+
+/*
+VARIABLES MUTATION AND COERCION
+*/
+
+var firstName = 'Rodolfo'
+var age = 28;
+
+console.log(firstName + ' ' +age); //TYPE COERCION
+
+var job, isMarried;
+job = 'Teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' +age + ' years old, ' + job + ' is he married?' + isMarried );
+
+age = 'twenty eight';
+job = 'Driver';
+
+alert(firstName + ' is a ' +age + ' years old, ' + job + ' is he married?' + isMarried );
+
+var lastName = prompt('What is his last name');
+console.log(firstName + lastName);
+
+
 
